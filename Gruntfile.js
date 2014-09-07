90db18cdc6a986b917af13465af7f3a5d156c5ef
Julian Tith <julian.tith@icloud.com>
2014-09-07 12:11:08 -0700

    Use grunt build tool

---
+++ Gruntfile.js
@@ -0,0 +1,19 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    stylus: {
+      compile: {
+        options: {
+          compress: false,
+          "include css": true
+        },
+        files: {
+          'master.css': 'css/stylus/main.styl'
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-stylus');
+  grunt.registerTask('default', ['stylus']);
+};
